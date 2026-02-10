e2bc5956c86828fb619d3b1dc3b3b390994e8e2c
ArpitaRai955586 <ariptarai@gmail.com>
2026-02-11 00:23:09 +0530

    update

--- recursion/recursion1.js
+++ recursion/recursion1.js
@@ -1,8 +1,9 @@
 let prompt=require('prompt-sync')()
 let n=Number(prompt("enter the number:"));
-for(let i=1;i<=n;i++){
-    for(let j=1;j<=i;j++){
-        process.stdout.write( "*")
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write(j + " ");
     }
     console.log();
 }
+
