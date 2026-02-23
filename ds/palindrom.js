47f0b119cf2b779dfc3514b2c4a30f3f29952521
Arpitarai955586 <ariptarai@gmail.com>
2026-02-23 23:34:29 +0530

    add

--- ds/palindrom.js
+++ ds/palindrom.js
@@ -1,14 +1,4 @@
- let prompt=require('prompt-sync')();
-// let n=Number(prompt("enter the number:"))
-// for(let i=1;i<=n;i++){
-//     for(let j=1;j<=n;j++){
-//         process.stdout.write("* ")
-//     }
-//     console.log();
-// }
-
-
-// palinderom number
+let prompt=require('prompt-sync')();
 let num=Number(prompt("enter the number:"))
 
 
