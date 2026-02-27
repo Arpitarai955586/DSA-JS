4c88775c7b6aa016238dfc64325564e41733b6e0
Arpitarai955586 <ariptarai@gmail.com>
2026-02-27 11:33:09 +0530

    freq

---
+++ string/frequency.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+const s = prompt("enter the string : ");
+
+let arr = new Array(128).fill(0); 
+
+for (let i = 0; i < s.length; i++) {
+    let indx = s.charCodeAt(i);
+    arr[indx] = arr[indx] + 1;
+}
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+        console.log(String.fromCharCode(i) + " appears " + arr[i] + " times");
+    }
+}
