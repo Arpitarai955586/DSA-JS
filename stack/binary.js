91e8c862609dac096e83a35eef6afb03b4e794cf
Arpitarai955586 <ariptarai@gmail.com>
2026-03-14 17:46:28 +0530

    binary

---
+++ stack/binary.js
@@ -0,0 +1,20 @@
+function binarySearch(arr,target){
+    let low=0
+    let high=arr.length-1
+
+    while(low<=high){
+        let mid=Math.floor((low+high)/2)
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[mid]<target){
+            low= mid+1
+        }
+        else{
+            high= mid -1
+        }
+    }
+    return -1;
+}
+let arr =[1,2,5,7,8,9,10]
+console.log(binarySearch(arr,7))
---
+++ stack/binary1.js
@@ -0,0 +1,18 @@
+function binarySearch(arr,target){
+    let low=0;
+    let high=arr.length-1;
+    while(low<=high){
+        let mid=Math.floor((low+high)/2)
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[mid]<target){
+            low=mid+1
+        }else{
+            high=mid-1
+        }
+    }
+    return -1
+}
+let arr=[2,3,4,7,18,36,78]
+console.log(binarySearch(arr,18))
